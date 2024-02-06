a74c1fdb7fd055669be0dd79f51f2fc6535ef4da
Tarnadas <Tarnadas@users.noreply.github.com>
2024-02-06 10:33:31 +0000

    deploy: 1251f7ebd53fe034838669fc410cc708f47763b2

---
+++ assets/index-b9xVyUO2.js
@@ -0,0 +1,9 @@
+import{aY as t,af as n,b0 as e,b1 as i,b3 as s,aZ as f,b2 as d,a$ as c,a_ as l}from"./index-Mc0SkGHt.js";const a=`
+@font-face {
+  font-family: 'Inter';
+  font-style:  normal;
+  font-weight: 300 600;
+  font-display: swap;
+  src: url("https://rsms.me/inter/font-files/Inter-roman.var.woff2?v=3.19") format("woff2");
+}
+`;export{a as InterVar,t as ProviderRpcError,n as ProviderRpcErrorCode,e as chainIdValidation,i as chainNamespaceValidation,s as chainValidation,f as createEIP1193Provider,d as providerConnectionInfoValidation,c as validate,l as weiToEth};
