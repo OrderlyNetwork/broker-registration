2dfef5a615ee29bbb4f76d07f47cf84ee0227cfd
Tarnadas <Tarnadas@users.noreply.github.com>
2024-05-24 09:32:02 +0000

    deploy: a0669281a1eb1944c2f2c5bf5ac198674773062c

---
+++ assets/index-CJr7u88G.js
@@ -0,0 +1,9 @@
+import{B as a}from"./index-BwjQci68.js";import{P as f,b as s,d,e as c,f as l,a as m,p,v}from"./index-BwjQci68.js";const t=`
+@font-face {
+  font-family: 'Inter';
+  font-style:  normal;
+  font-weight: 300 600;
+  font-display: swap;
+  src: url("https://rsms.me/inter/font-files/Inter-roman.var.woff2?v=3.19") format("woff2");
+}
+`;function n(r){return new a(r).div(1e18).toString(10)}export{t as InterVar,f as ProviderRpcError,s as ProviderRpcErrorCode,d as chainIdValidation,c as chainNamespaceValidation,l as chainValidation,m as createEIP1193Provider,p as providerConnectionInfoValidation,v as validate,n as weiToEth};
--- assets/index-OW3MSRIc.js
+++
@@ -1,9 +0,0 @@
-import{P as t,b as e,d as n,e as i,f as s,a as f,p as d,v as c,w as l}from"./index-SwEWUE7-.js";const a=`
-@font-face {
-  font-family: 'Inter';
-  font-style:  normal;
-  font-weight: 300 600;
-  font-display: swap;
-  src: url("https://rsms.me/inter/font-files/Inter-roman.var.woff2?v=3.19") format("woff2");
-}
-`;export{a as InterVar,t as ProviderRpcError,e as ProviderRpcErrorCode,n as chainIdValidation,i as chainNamespaceValidation,s as chainValidation,f as createEIP1193Provider,d as providerConnectionInfoValidation,c as validate,l as weiToEth};
