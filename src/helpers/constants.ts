fd9fceb2572bf3b99ecbbce4efbe24fa75fb4daf
Mario Reder <mario.reder@pm.me>
2024-06-13 11:36:05 +0200

    fix base chain support

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,69 +1,54 @@
+import { match } from 'ts-pattern';
+
+type SupportedChainIds =
+  | '0xa4b1' // arbitrum
+  | '0xa' // optimism
+  | '0x2105' // base
+  | '0x66eee' // arbitrum sepolia
+  | '0xaa37dc' // optimism sepolia
+  | '0x14a34'; // base sepolia
+
 export function isTestnet(chainId: string): boolean {
-  switch (chainId) {
-    case '0xa4b1':
-    case '0xa':
-      return false;
-    case '0x66eee':
-    case '0xaa37dc':
-      return true;
-    default:
-      return false;
-  }
+  return match(chainId as SupportedChainIds)
+    .with('0xa4b1', '0xa', '0x2105', () => false)
+    .with('0x66eee', '0xaa37dc', '0x14a34', () => true)
+    .exhaustive();
 }
 
 export function getVaultAddress(chainId: string): string {
-  switch (chainId) {
-    case '0xa4b1':
-      return '0x816f722424B49Cf1275cc86DA9840Fbd5a6167e9';
-    case '0xa':
-      return '0x816f722424b49cf1275cc86da9840fbd5a6167e9';
-    case '0x66eee':
-      return '0x0EaC556c0C2321BA25b9DC01e4e3c95aD5CDCd2f';
-    case '0xaa37dc':
-      return '0xEfF2896077B6ff95379EfA89Ff903598190805EC';
-    default:
-      throw new Error('chain ID unsupported');
-  }
+  return match(chainId as SupportedChainIds)
+    .with('0xa4b1', () => '0x816f722424B49Cf1275cc86DA9840Fbd5a6167e9')
+    .with('0xa', () => '0x816f722424b49cf1275cc86da9840fbd5a6167e9')
+    .with('0x2105', () => '0x816f722424b49cf1275cc86da9840fbd5a6167e9')
+    .with('0x66eee', () => '0x0EaC556c0C2321BA25b9DC01e4e3c95aD5CDCd2f')
+    .with('0xaa37dc', () => '0xEfF2896077B6ff95379EfA89Ff903598190805EC')
+    .with('0x14a34', () => '0xdc7348975aE9334DbdcB944DDa9163Ba8406a0ec')
+    .exhaustive();
 }
 
 export function getVerifyingAddress(chainId: string): string {
-  switch (chainId) {
-    case '0xa4b1':
-    case '0xa':
-      return '0x6F7a338F2aA472838dEFD3283eB360d4Dff5D203';
-    case '0x66eee':
-    case '0xaa37dc':
-      return '0x1826B75e2ef249173FC735149AE4B8e9ea10abff';
-    default:
-      throw new Error('chain ID unsupported');
-  }
+  return match(isTestnet(chainId))
+    .with(false, () => '0x6F7a338F2aA472838dEFD3283eB360d4Dff5D203')
+    .with(true, () => '0x1826B75e2ef249173FC735149AE4B8e9ea10abff')
+    .exhaustive();
 }
 
 export function getUSDCAddress(chainId: string): string {
-  switch (chainId) {
-    case '0xa4b1':
-      return '0xaf88d065e77c8cC2239327C5EDb3A432268e5831';
-    case '0xa':
-      return '0x816f722424b49cf1275cc86da9840fbd5a6167e9';
-    case '0x66eee':
-      return '0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d';
-    case '0xaa37dc':
-      return '0x5fd84259d66Cd46123540766Be93DFE6D43130D7';
-    default:
-      throw new Error('chain ID unsupported');
-  }
+  return match(chainId as SupportedChainIds)
+    .with('0xa4b1', () => '0xaf88d065e77c8cC2239327C5EDb3A432268e5831')
+    .with('0xa', () => '0x0b2c639c533813f4aa9d7837caf62653d097ff85')
+    .with('0x2105', () => '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913')
+    .with('0x66eee', () => '0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d')
+    .with('0xaa37dc', () => '0x5fd84259d66Cd46123540766Be93DFE6D43130D7')
+    .with('0x14a34', () => '0x036CbD53842c5426634e7929541eC2318f3dCF7e')
+    .exhaustive();
 }
 
 export function getBaseUrl(chainId: string): string {
-  switch (chainId) {
-    case '0xa4b1':
-    case '0xa':
-      return 'https://api-evm.orderly.org';
-    case '0x66eee':
-    case '0xaa37dc':
-    default:
-      return 'https://testnet-api-evm.orderly.org';
-  }
+  return match(isTestnet(chainId))
+    .with(false, () => 'https://api-evm.orderly.org')
+    .with(true, () => 'https://testnet-api-evm.orderly.org')
+    .exhaustive();
 }
 
 export type EIP712Domain = {
