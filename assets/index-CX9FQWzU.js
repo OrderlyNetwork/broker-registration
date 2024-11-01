4f6e501e508ea6e6f3d4a5131fb92a931557285c
Tarnadas <Tarnadas@users.noreply.github.com>
2024-11-01 06:34:46 +0000

    deploy: 79cb975258dc121bc276a9eb1c0baeaf602906ab

--- assets/index-CX9FQWzU.js
+++ assets/index-CX9FQWzU.js
@@ -1,4 +1,4 @@
-import{B as a}from"./index-CV6lwyN0.js";import{P as f,b as s,d,e as c,f as l,a as m,p,v}from"./index-CV6lwyN0.js";const t=`
+import{B as a}from"./index-6tidtOLj.js";import{P as f,b as s,d,e as c,f as l,a as m,p,v}from"./index-6tidtOLj.js";const t=`
 @font-face {
   font-family: 'Inter';
   font-style:  normal;
