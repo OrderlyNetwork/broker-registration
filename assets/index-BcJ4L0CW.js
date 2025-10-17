c15f25afb4eb9ffc1a206ba4d093bc2f16c18d41
Tarnadas <Tarnadas@users.noreply.github.com>
2025-10-17 10:09:18 +0000

    deploy: a143e657db1640a2dccde9c9a3cd44c275ade700

--- assets/index-BcJ4L0CW.js
+++ assets/index-BcJ4L0CW.js
@@ -1,4 +1,4 @@
-import{B as a}from"./index-BY51L-wH.js";import{P as f,b as s,d,e as c,f as l,a as m,p,v}from"./index-BY51L-wH.js";const t=`
+import{B as a}from"./index-AyxngnCi.js";import{P as f,b as s,d,e as c,f as l,a as m,p,v}from"./index-AyxngnCi.js";const t=`
 @font-face {
   font-family: 'Inter';
   font-style:  normal;
